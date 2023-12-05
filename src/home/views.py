cab4e05431e9873518c06337c15b0827affb9297
MARK <mehdimirzaie1378@gmail.com>
2023-12-05 19:59:18 +0330

    home | views.py | Updated with information of about application

--- src/home/views.py
+++ src/home/views.py
@@ -4,10 +4,16 @@ from .models import User
 
 
 def index(request):
-    user = User.objects.filter(is_portfolio=True).select_related('background').prefetch_related('links', 'titles')
+    user = User.objects.filter(is_portfolio=True).select_related('background', 'about', 'fact').prefetch_related(
+        'links', 'titles')
     if user.exists():
         user = user.last()
         links = user.links.all()
         titles = ', '.join(str(title) for title in user.titles.all())
-        return render(request, 'index.html', {'user': user, 'links': links, 'titles': titles})
+        about = user.about
+        fact = user.fact
+        skills = user.skills.all()
+
+        context = {'user': user, 'links': links, 'titles': titles, 'about': about, 'fact': fact, 'skills': skills}
+        return render(request, 'index.html', context)
     return HttpResponse("<h1>No Portfolio Found!</h1>")
