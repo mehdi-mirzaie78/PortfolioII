c02270d9093a7f599fe7aedb282dbb70fd783ad0
MARK <mehdimirzaie1378@gmail.com>
2023-12-05 16:54:55 +0330

    home | views.py | Modified index view to show portfolio user

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import User
 
 
 def index(request):
-    user = User.objects.filter(is_active=True).select_related('background').prefetch_related('links', 'titles').first()
-    links = user.links.all()
-    titles = ', '.join(str(title) for title in user.titles.all())
-    return render(request, 'index.html', {'user': user, 'links': links, 'titles': titles})
+    user = User.objects.filter(is_portfolio=True).select_related('background').prefetch_related('links', 'titles')
+    if user.exists():
+        user = user.last()
+        links = user.links.all()
+        titles = ', '.join(str(title) for title in user.titles.all())
+        return render(request, 'index.html', {'user': user, 'links': links, 'titles': titles})
+    return HttpResponse("<h1>No Portfolio Found!</h1>")
