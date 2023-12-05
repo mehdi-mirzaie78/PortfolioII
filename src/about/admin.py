ecc978b9d07aaf3555b8cb47f666454c806cc7d0
MARK <mehdimirzaie1378@gmail.com>
2023-12-05 19:56:47 +0330

    about | admin.py | Added Inlines of About, Fact, Skill

--- src/about/admin.py
+++ src/about/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import About, Fact, Skill
 
-# Register your models here.
+
+class AboutInline(admin.StackedInline):
+    classes = ['collapse']
+    model = About
+    readonly_fields = ('image_tag',)
+    extra = 0
+
+
+class FactInline(admin.TabularInline):
+    classes = ['collapse']
+    model = Fact
+    extra = 0
+
+
+class SkillInline(admin.TabularInline):
+    classes = ['collapse']
+    model = Skill
+    extra = 1
+
