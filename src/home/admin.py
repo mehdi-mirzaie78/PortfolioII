51b0a07a80aeaa7345b8e5d8b399f4414658db24
MARK <mehdimirzaie1378@gmail.com>
2023-12-05 19:58:01 +0330

    home | admin.py | Modified UserAdmin with inlines of about application

--- src/home/admin.py
+++ src/home/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from . import models
+from about.admin import AboutInline, FactInline, SkillInline
 
 admin.site.unregister(Group)
 
 
+# ---------------------------------- Home ---------------------------------- #
 class LinkInline(admin.TabularInline):
     classes = ['collapse']
     model = models.Link
@@ -27,15 +29,15 @@ class TitleInline(admin.TabularInline):
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'full_name', 'image_tag', 'is_portfolio']
-    inlines = [TitleInline, LinkInline, BackgroundInline]
+    inlines = [
+        TitleInline, LinkInline, BackgroundInline, AboutInline, FactInline, SkillInline
+    ]
     readonly_fields = ('image_tag', 'password', 'last_login', 'date_joined')
     fieldsets = (
         ('General Information', {'fields': ('username', 'email', ('first_name', 'last_name', 'is_portfolio'))}),
-        ('Optional', {
+        ('Image Information', {
             'fields': (('image_tag', 'image'),),
             'classes': ('collapse',)
 
         }),
     )
-
-
