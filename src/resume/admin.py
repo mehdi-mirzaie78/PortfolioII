c3d7a3f1055b060809a22a35dea6cf5f705019a5
MARK <mehdimirzaie1378@gmail.com>
2023-12-07 19:26:06 +0330

    resume | admin.py | Added necessary model inlines & admin

--- src/resume/admin.py
+++ src/resume/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Summary, Education, ProfessionalExperience, CV, ProfessionItem
 
-# Register your models here.
+
+class SummaryInline(admin.TabularInline):
+    classes = ['collapse']
+    model = Summary
+    extra = 0
+
+
+class EducationInline(admin.StackedInline):
+    classes = ['collapse']
+    model = Education
+    extra = 0
+
+
+class ProfessionItemInline(admin.TabularInline):
+    classes = ['collapse']
+    model = ProfessionItem
+    extra = 0
+
+
+class ProfessionalExperienceInline(admin.TabularInline):
+    classes = ['collapse']
+    model = ProfessionalExperience
+    extra = 0
+    show_change_link = True
+
+
+@admin.register(ProfessionalExperience)
+class ProfessionalExperienceAdmin(admin.ModelAdmin):
+    inlines = [ProfessionItemInline]
+
+
+class CVInline(admin.TabularInline):
+    classes = ['collapse']
+    model = CV
+    extra = 0
