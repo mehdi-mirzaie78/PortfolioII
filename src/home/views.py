4928c08f064c12d73a2e69b148abe2e2e89889b0
MARK <mehdimirzaie1378@gmail.com>
2023-12-07 19:29:18 +0330

    home | views.py | Modified index with data of resume section

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import User
 
 
@@ -13,7 +14,13 @@ def index(request):
         about = user.about
         fact = user.fact
         skills = user.skills.all()
+        summary = user.summary
+        educations = user.education.all()
+        professional_experiences = user.professional_experiences.prefetch_related('items').all()
 
-        context = {'user': user, 'links': links, 'titles': titles, 'about': about, 'fact': fact, 'skills': skills}
+        context = {'user': user, 'links': links, 'titles': titles,
+                   'about': about, 'fact': fact, 'skills': skills,
+                   'summary': summary, 'educations': educations, 'professional_experiences': professional_experiences,
+                   }
         return render(request, 'index.html', context)
     return HttpResponse("<h1>No Portfolio Found!</h1>")
