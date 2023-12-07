064a997d08d85ec07f4d1d00bae80fe382422a92
MARK <mehdimirzaie1378@gmail.com>
2023-12-07 19:30:47 +0330

    home | urls.py | Added download-cv path

--- src/home/urls.py
+++ src/home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('download-cv/', views.download_cv, name='download_cv'),
+
 ]
