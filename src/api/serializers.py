8a6fc7cbc82ca3a2b59162e43cdbef85874f14aa
mahdi <mehdimirzaie1378@gmail.com>
2023-12-12 12:54:18 +0330

    api | serializers.py | Added MessageSerialier

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from home.models import User
 from portfolio.models import Project
+from contact.models import Message
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -8,3 +10,20 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ["id", "title", "link", "video", "description", "skills"]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ["name", "email", "subject", "message"]
+
+    def save(self, user):
+        message = Message(
+            name=self.validated_data["name"],
+            email=self.validated_data["email"],
+            subject=self.validated_data["subject"],
+            message=self.validated_data["message"],
+            user=user,
+        )
+        message.save()
+        return message
